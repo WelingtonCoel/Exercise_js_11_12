1394c1f565e818f921afbf4a33b31de5b2d76bbe
WelingtonCoel <welingtonc12@gmail.com>
2023-08-14 21:03:10 +0100

    Simple exercise js_06

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var vel = 44 
+console.log (`Sua velocidade é ${vel} km/h`)
+
+if ( vel >= 50) //condição simples
+{
+    console.log('Velocidade execedida, MULTADO')
+}
+
+console.log('Continue em velocidade segura, Boa Viagem')
---
+++ ex009.js
@@ -0,0 +1,12 @@
+var stt = 'Brasil'
+
+console.log(`Vivendo no ${stt}`)
+
+if (stt == 'Brasil')
+{
+    console.log('Brasileiro')
+}
+else {
+    console.log('estrangeiro')
+}
+   
