cad48cc0519f9e4e6c0d08c76e487761b23d98c0
WelingtonCoel <welingtonc12@gmail.com>
2023-08-16 00:06:32 +0100

    Exercise_14

---
+++ Aula_13/ex014.js
@@ -0,0 +1,14 @@
+var num = 15
+
+//if (num > 10){
+   // console.log(`Unespected number ${num}`)
+    
+    while (num != 0){
+        if (num > 10){
+            console.log(`Unespected number ${num}`)
+      console.log(`testa ${num}`)
+    num --
+
+     
+    }
+}
