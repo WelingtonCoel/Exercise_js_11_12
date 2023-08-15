1fb12a5006db3c0876e2b931bb8be5b8770bdb64
WelingtonCoel <53539574+WelingtonCoel@users.noreply.github.com>
2023-08-15 22:31:57 +0100

    Add files via upload

---
+++ ex012.js
@@ -0,0 +1,17 @@
+// Mensagem conforme HORA
+
+var hora = 15
+
+console.log (`Agora são ${hora} h!`)
+
+if ( hora < 12 ) { 
+
+    console.log('Bom dia')
+
+} else if (hora <= 18 ) {
+     console.log('Boa tarde')
+
+    }    else {
+        console.log('Boa noite')
+}
+ 
